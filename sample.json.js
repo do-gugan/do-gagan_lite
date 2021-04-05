17953e3b2f2162bb2e0262878a446443f8bc6250
Kazuyoshi Furuta <furuta@do-gugan.com>
2021-04-06 00:00:43 +0900

    1st sample file.

---
+++ sample.json.js
@@ -0,0 +1,8 @@
+const scriptsJson = [
+    { in:4,script:"こんにちは",speaker:0},
+    { in:6,script:"こんにちは",speaker:1},
+    { in:7,script:"本日は調査にご協力ありがとうございます。",speaker:0},
+    { in:10,script:"本日進行を担当させていただきます古田と申します。",speaker:0},
+    { in:13,script:"どうぞ、よろしくお願いいたします。。",speaker:0},
+    { in:15,script:"よろしくお願いいたします。。",speaker:1}
+];
